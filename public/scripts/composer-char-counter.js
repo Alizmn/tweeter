359b9d1b86b9da07cb68df973e0b02931a29d5e1
Ali Zamani <Ali.zmn@hotmail.com>
2020-12-09 21:58:08 +0000

    Added char count and coloring

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+  $('#tweet-text').on('input', event => {
+    let charCount = 140 - $(event.target).val().length;
+    $('form .counter').html(charCount)
+    if (charCount < 0) {
+      $('form .counter').css("color", "red")
+    } else if (charCount <= 40) {
+      $('form .counter').css("color", "orange")
+    } else {
+      $('form .counter').css("color", "black")
+    }
+    // console.log(charCount);
+  })
+})
+
