92662c202787f6a26c607e7c9ef00df60ecd41b4
Ali Zamani <Ali.zmn@hotmail.com>
2020-12-10 22:33:27 +0000

    ajax get and post structured but not complete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,30 +7,6 @@
  // Test / driver code (temporary). Eventually will get this from the server.
  
  $(document).ready(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
   const createTweetElement = (data) => {
     const tweet = (`<article>
@@ -61,7 +37,17 @@
       $('#tweet-container').append(createTweetElement(element))
     });
   }
-  renderTweets(data);
+
+  const loadTweets = () => {
+    $
+      .ajax({
+        url: "/tweets",
+        method: "GET"
+      })
+      .then(res => renderTweets(res))
+  }
+  loadTweets()
+  // renderTweets(data);
   $('form').on('submit', (event) => {
     event.preventDefault();
     $
